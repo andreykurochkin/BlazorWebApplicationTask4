1880742d684aa1822dedcafecac48a2749992dbf
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-09-16 06:05:50 +0300

    beforee adding identity

---
+++ BlazorWebApplicationTask4/Areas/Identity/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebApplicationTask4.Areas.Identity.Data {
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ BlazorWebApplicationTask4/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebApplicationTask4.Areas.Identity.Data {
+    public class ApplicationUser : IdentityUser {
+        public string SocialName { get; set; }
+        public DateTime FirstLoginDate { get; set; }
+        public DateTime LastLogoutDate { get; set; }
+        public string Status { get; set; }
+    }
+}
