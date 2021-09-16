929cdd40650a934668cfadd05b0c6a56df29005b
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-09-16 06:16:10 +0300

    after identity scaffolding

---
+++ BlazorWebApplicationTask4/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BlazorWebApplicationTask4.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorWebApplicationTask4.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorWebApplicationTask4.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
